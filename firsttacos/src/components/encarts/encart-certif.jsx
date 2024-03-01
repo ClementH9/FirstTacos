4e78cc04f46f22b4c6e9869030005fba2f40ff35
ClementH9 <henot.clement@gmail.com>
2024-03-01 15:22:02 +0100

    halal

--- firsttacos/src/components/encarts/encart-certif.jsx
+++ firsttacos/src/components/encarts/encart-certif.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const EncartCertif = () => {
     return (
 
-<section className="encart encart-certif">
-    <img className="halal" src='./Assets/halal-food.png' alt="Certifié Halal" />
-    <p className="infos-livraison">Tous nos produits sont certifiés Halal</p>
+<section class="encart encart-certification">
+    <h2>Certificat Halal</h2>
+    <p class="infos-livraison">Tous nos produits sont certifiés Halal</p>
 </section>
 
     )
