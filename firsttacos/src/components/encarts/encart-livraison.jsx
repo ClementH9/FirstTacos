a8e4dfb43fe677da4afcb6b35b210b423b35fe03
ClementH9 <henot.clement@gmail.com>
2024-03-02 03:56:50 +0100

    enlever html classname

--- firsttacos/src/components/encarts/encart-livraison.jsx
+++ firsttacos/src/components/encarts/encart-livraison.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTruck } from '@fortawesome/free-solid-svg-icons';
 
 const EncartCommande = () => {
     return (
 
 <section className="encart encart-livraison">
-    <h2>Livraison</h2>
+    <h2><FontAwesomeIcon icon={faTruck} /> Livraison</h2>
     <p className="infos-livraison">Faites-vous livrer GRATUITEMENT en 45 minutes</p>
     <p className="lieux-livraison">Nous livrons le soir sur Croix et Wasquehal (à partir de 13€)</p>
 </section>
