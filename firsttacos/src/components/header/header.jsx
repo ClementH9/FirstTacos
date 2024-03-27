a13a0322c65268bf423fa9818a0248e1c5616649
ClementH9 <henot.clement@gmail.com>
2024-03-27 18:52:02 +0100

    update 27/3

--- firsttacos/src/components/header/header.jsx
+++ firsttacos/src/components/header/header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import logo from "../../Assets/logo-ft.png";
 
 const Header = () => {
     return (
     <header>
-        <h1>FIRST TACOS</h1>
+        <img src={logo} alt="First Tacos" className="logo"/>
         <p className="adresse"><FontAwesomeIcon icon={faLocationDot} /> 5 rue Jean Jaurès, 59290 Wasquehal</p>
     </header>
     )
